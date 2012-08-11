bf1f1a5e86952f50645f50d68c5890ebf117366c
Suresh Alse <alseambusher@gmail.com>
2012-08-12 02:57:12 +0530

    basic

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?
-$base_url='http://localhost/music_composer';
+if ( ! defined("BASEPATH")) exit("No direct script access allowed");
+$config= array();
+$config['base_url']='http://localhost/music_composer';
+$config['db_host']='localhost';
+$config['db_username']='root';
+$config['db_password']='alse';
+$config['db_database']='cloud_music';
 ?>
--- includes/bootstrap.inc.php
+++ includes/bootstrap.inc.php
@@ -1,12 +1,8 @@
 <?
-//$baseUrl='http://'.$_SERVER['SERVER_NAME'].'/indibits_wiki/';
-include('config.php');
-$baseUrl=$this->config->base_url();
-$bootstrap=$baseUrl."bootstrap/";
+//you have to include config file before itself
+$bootstrap=$config['base_url']."/bootstrap/";
 echo '<link href="'.$bootstrap.'css/'.$theme.'.css" rel="stylesheet">';
-//echo '<link href="'.$bootstrap.'css/bootstrap.min.css" rel="stylesheet">';
 echo '<link href="'.$bootstrap.'css/bootstrap-responsive.css" rel="stylesheet">';
-//echo '<link href="'.$bootstrap.'css/bootstrap-responsive.min.css" rel="stylesheet">';
 echo "<script type='text/javascript' src='".$bootstrap."js/jquery.js'></script>";
 echo "<script type='text/javascript' src='".$bootstrap."js/bootstrap.js'></script>";
 echo "<script type='text/javascript' src='".$bootstrap."js/bootstrap-popover.js'></script>";
